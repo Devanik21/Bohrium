a6f5bacbb4582b9be07660e3f98a49bbcb5c2c79
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-11-10 15:39:14 +0530

    Update ReSeArcH.py

--- ReSeArcH.py
+++ ReSeArcH.py
@@ -271,7 +271,7 @@ def save_state():
 # Configure Gemini API
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel('gemini-2.5-flash')
+    model = genai.GenerativeModel('gemma-3-27b-it)
 except Exception as e:
     st.error("⚠️ Please configure GEMINI_API_KEY in Streamlit secrets")
 
